aa2067d4f93a9174401d2a0e2c4958521b49197c
sweakleyw <sweakleyworks@gmail.com>
2023-05-05 01:31:50 -0400

    added paths

--- app.js
+++ app.js
@@ -1,9 +1,90 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("public"));
 
-const PORT = 1337;
+app.use(morgan("dev"));
+
+app.get("/", (req, res, next) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts
+          .map(
+            (post) => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              <a href="/posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | Posted: ${post.date}
+            </small>
+          </div>`
+          )
+          .join("")}
+    </div>
+  </body>
+  </html>`;
+
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  if (!post.id) {
+    next(error);
+  }
+
+  if (post.id) {
+    const postHtml = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+            ${post.content}
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div> 
+   </div>
+</body>
+</html>`;
+    res.send(postHtml);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(404).send(`<h1>404: Not Found!</h1><p>Go back</p>`);
+});
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
